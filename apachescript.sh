c44079bace59e3d89218f996ef05bf8278d77153
avyanab <avyanab@gmail.com>
2023-08-13 19:18:15 +0000

    update apache installation to fix firewall issues

--- apachescript.sh
+++ apachescript.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-#Updating all packages and installing Apache
-
+#Update system packages
 sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
 
-#Script for a customized apache webpage
+#Install and start Apache
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+systemctl is-enabled httpd
+
+#Install of firewalld service on Amazon Linux 2
+sudo yum install -y firewalld
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo systemctl status firewalld
+
+#Configure firewall to allow services HTTP and HTTPS
+sudo firewall-cmd --zone=public --add-service=http --permanent
+sudo firewall-cmd --zone=public --add-service=https --permanent
+
+#Reload firewall to implement added services
+sudo firewall-cmd --reload
+sudo firewall-cmd --state
+
+#Customize default index page for Apache
 
 cd /var/www/html
 
-sudo echo "<html><body><h1>Welcome to my webpage!</h1></body></html>" > /var/www/html/index.html
+sudo echo "<html><body><h1>Welcome to my Apache webpage!</h1></body></html>" > /var/www/html/index.html
